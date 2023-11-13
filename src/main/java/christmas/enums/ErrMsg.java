959cad80ee9fbe52fc269144a19b28686c165400
dmdwns2 <ssreki1@naver.com>
2023-11-14 03:47:10 +0900

    feat: Create error messages and input messages enum - Input messages are treated as constants for multilingual purposes

---
+++ src/main/java/christmas/enums/ErrMsg.java
@@ -0,0 +1,13 @@
+package christmas.enums;
+
+public enum ErrMsg {
+    EMPTY_INPUT("[ERROR] 내용을 입력해 주세요."),
+    INVALID_ORDER("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    INVALID_DATE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+
+    private final String message;
+
+    ErrMsg(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/christmas/enums/InputMsg.java
@@ -0,0 +1,12 @@
+package christmas.enums;
+
+public enum InputMsg {
+    READ_DATE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
+    READ_ORDER("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+
+    private final String message;
+
+    InputMsg(String message) {
+        this.message = message;
+    }
+}
