8d1ffd2edabc8ee45c2961ce77b1c378dc63c098
dmdwns2 <ssreki1@naver.com>
2023-11-15 00:05:44 +0900

    feat: create eventbadge enum

---
+++ src/main/java/christmas/enums/EventBadge.java
@@ -0,0 +1,29 @@
+package christmas.enums;
+
+public enum EventBadge {
+    NONE("없음", 0),
+    STAR("별", 5_000),
+    TREE("트리", 10_000),
+    SANTA("산타", 20_000);
+
+    private final String name;
+    private final int discountPrice;
+
+    EventBadge(String name, int discountPrice) {
+        this.name = name;
+        this.discountPrice = discountPrice;
+    }
+
+    public static EventBadge getEventBadge(int price) {
+        if (price >= SANTA.discountPrice) {
+            return SANTA;
+        }
+        if (price >= TREE.discountPrice) {
+            return TREE;
+        }
+        if (price >= STAR.discountPrice) {
+            return STAR;
+        }
+        return NONE;
+    }
+}
